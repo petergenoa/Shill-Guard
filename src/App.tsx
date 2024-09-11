036e85e44f023bbe18e5dc17229b4f0dacb6e48e
Petar Genov <genov1824@gmail.com>
2024-09-11 16:59:54 +0300

    Remove not needed code

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,11 @@ import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
 
-const StyledApp = styled.div`
-  background-color: #e8e8e8;
-  color: black;
-
-  @media (prefers-color-scheme: dark) {
-    background-color: #222;
-    color: white;
-  }
-  min-height: 100vh;
-  padding: 20px 20px;
-`;
-
-const AppContainer = styled.div`
-  max-width: 900px;
-  margin: 0 auto;
-`;
-
 function App() {
   const { network } = useTonConnect();
 
   return (
-    <StyledApp>
-      <AppContainer>
+
         <FlexBoxCol>
           <FlexBoxRow>
             <TonConnectButton />
@@ -43,12 +25,7 @@ function App() {
                 : "N/A"}
             </Button>
           </FlexBoxRow>
-          <Counter />
-          <TransferTon />
-          <Jetton />
         </FlexBoxCol>
-      </AppContainer>
-    </StyledApp>
   );
 }
 
