97286e72aa313dfcb0c7ee0e0ab19bbe6ba20bae
Petar Genov <genov1824@gmail.com>
2024-10-27 03:52:24 +0300

    add firebase, push new screens and looks

--- src/pages/More.tsx
+++ src/pages/More.tsx
@@ -2,14 +2,33 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import "./styles/More.css";
 import { useUserContext } from "../contexts/UserContext";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { useTonConnect } from "../hooks/useTonConnect";
+import { useEffect } from "react";
 
 const More = () => {
     const { user, updateUser } = useUserContext();
+    const { connected, wallet } = useTonConnect();
+
     const handleShare = () => {
         const message = `Check out Gangster Games! Join through this link: https://t.me/https://t.me/ShillGuardAppBot/Shill-Guard?startapp=refId${user?.userId}`;
         const url = `https://t.me/share/url?url=${encodeURIComponent(message)}`;
         window.Telegram.WebApp.openTelegramLink(url);
     };
+
+    useEffect(() => {
+        if (user && !user.earnInfo.tonWalletConnected && connected) {
+          const newCoins = user.coins + 500000;
+          updateUser({
+            ...user,
+            coins: newCoins,
+            earnInfo: {
+              ...user.earnInfo,
+              tonWalletConnected: true,
+              tonWallet: wallet ?? '',
+            }
+          });
+        }
+    }, [connected]);
 
     if(!user) {
         return;
